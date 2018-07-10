e94752cdedb487aa6224b7379746663ba08691a2
mwicigi <mwicigimoses@gmail.com>
2018-07-10 13:03:32 +0300

    my setup

--- application/views/user/includes/template.php
+++
@@ -1,9 +0,0 @@
-<?php $this->load->view('user/includes/header'); ?>
-
-<div id="main_content">
-
-<?php $this->load->view($main_content);?>
-
-</div>
-
-<?php $this->load->view('user/includes/footer'); ?>
