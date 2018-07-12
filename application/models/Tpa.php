295a5e6cf73ccbc2172d5b55b1e20a8942a446b7
mwicigi <mwicigimoses@gmail.com>
2018-07-12 14:58:23 +0300

    project integration

--- application/models/Tpa.php
+++ application/models/Tpa.php
@@ -16,11 +16,12 @@
          $query = $this->db->get('blog',3);
          return $query->result();
       }
-      function get_projects(){
-        $this->db->order_by("id","desc");
-        $query = $this->db->get('project');
-        return $query->result();
-     }
+      function get_projects($project_id){
+        $this->db->from('project');
+           $this->db->where(array('project_id'=>$project_id));
+           return $this->db->get()->result();
+        }
+
      function get_projectlist(){
        $this->db->order_by("id","desc");
        $query = $this->db->get('project');
